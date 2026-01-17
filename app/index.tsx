8661460538ac0d5e07b52f6c134ed73f93411894
luca-waldvogel <waldvogel.luca@hotmail.com>
2026-01-17 11:42:59 +0100

    chore(MP3-2): Use EXPO_PUBLIC_* envs for native compatibility (this is unsafe but okay for this not-to-be-published project)

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,68 @@
-import {Button, Text, View} from "react-native";
-import { Link } from 'expo-router';
-import { commonStyles } from '@/styles/common';
+import { useState} from 'react';
+import { Text, View} from "react-native";
+import { Link, useRouter} from 'expo-router';
+import {commonStyles} from '@/styles/common';
+import {firebaseConfig} from '@/components/firebase-config'
+import {initializeApp} from "firebase/app";
+import {getAuth, signInWithEmailAndPassword} from "firebase/auth";
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+
+function SignInForm() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const router = useRouter();
+
+    function handleEmail(e: any) {
+        setEmail(e.target.value);
+    }
+
+    function handlePW(e: any) {
+        setPassword(e.target.value);
+    }
+
+    function handleSubmit(e: any) {
+        signInWithEmailAndPassword(auth, email, password)
+            .then(() => {
+                router.navigate('/(tabs)/converter')
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(errorCode, errorMessage);
+            });
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label>Email:
+                <input
+                    type="text"
+                    value={email}
+                    onChange={handleEmail}
+                />
+            </label>
+            <label>Password:
+                <input
+                    type="text"
+                    value={password}
+                    onChange={handlePW}
+                />
+            </label>
+            <input type="submit" />
+        </form>
+    )
+}
 
 export default function Index() {
   return (
     <View
       style={commonStyles.container}
     >
-      <Text style={commonStyles.text}>Login Page</Text>
-      <Button title="Login" onPress={() => {}} />
+      <SignInForm/>
+
         <Link href="/(tabs)/converter" dismissTo>
             <Text>Login(Mock)</Text>
         </Link>
